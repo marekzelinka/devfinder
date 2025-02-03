04d595add76630a48afe9cf67ee3be5244368fea
Marek Zelinka <mzelinka17@gmail.com>
2025-02-03 18:51:11 +0100

    fix layout stying

--- app/layouts/search.tsx
+++ app/layouts/search.tsx
@@ -9,12 +9,15 @@ export default function SearchLayout() {
         <div className="mx-auto max-w-3xl px-2 sm:px-4 lg:px-8">
           <div className="relative flex items-center justify-between py-5">
             <div className="flex items-center max-lg:px-2">
-              <div className="flex-none">
-                <Logo className="h-8 w-auto text-sky-500" />
+              <div className="flex flex-none items-center gap-3">
+                <Logo className="h-6 w-auto text-sky-500" />
+                <span className="text-base font-medium text-white max-md:hidden">
+                  DevFinder
+                </span>
               </div>
             </div>
             <div className="flex flex-1 justify-end px-2 md:ml-6">
-              <search role="search" className="w-full max-w-lg md:max-w-xs">
+              <search role="search" className="w-full md:max-w-xs">
                 <SearchBar />
               </search>
             </div>
